0a95fd3bed912391e7c3935b5f1ce6cf11e33dd7
hidayatnurdin71 <hidayatnurdin71@gmail.com>
2019-05-09 19:15:49 +0700

    menambah team.php untuk mengelist anggota kelompok

---
+++ team.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Team</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>nama kelompok   :</h1>
+    <h2>nurdin hidayat</h2>
+    <h2>abdi rahmat</h2>
+    <h2>m andika kurniawan</h2>
+</body>
+</html>
